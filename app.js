84d90fac5c6d378124f35d12dc8aa16c1d04919b
Alexandr <alerusford@gmail.com>
2020-11-13 13:45:30 +0300

    57:00

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 // 1 переменные
-
 const firstName = 'Alexandr'
 const lastName = 'Ivanov'
-const age = 29
+//const age = 29
 const isProgrammer = true
 
 // 2 Мутирование
 //console.log('Имя человека: ' + firstName + ', а возраст человека: ' + age)
 
 //3 Операторы
+const currentYear = 2020
+const birthYear = 1991
+//const age = currentYear - birthYear
+//console.log(age)
+
 
