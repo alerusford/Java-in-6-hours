4930b52cc60d8760c329db4ef14573150b50874b
Alexandr <alerusford@gmail.com>
2020-11-13 15:00:25 +0300

    условные_операторы

--- app.js
+++ app.js
@@ -1,16 +1,39 @@
 // 1 переменные
-const firstName = 'Alexandr'
-const lastName = 'Ivanov'
+// const firstName = 'Alexandr'
+// const lastName = 'Ivanov'
 //const age = 29
-const isProgrammer = true
+//const isProgrammer = true
 
 // 2 Мутирование
 //console.log('Имя человека: ' + firstName + ', а возраст человека: ' + age)
 
-//3 Операторы
-const currentYear = 2020
-const birthYear = 1991
+// 3 Операторы
+// const currentYear = 2020
+// const birthYear = 1991
 //const age = currentYear - birthYear
 //console.log(age)
+// const a = 10
+// const b = 5
+// let c = 32
+//c = c + a
+// c += a
+// console.log(c)
 
+// 4 типы данных
+// const isProgrammer = true
+// const name = 'Alexandr'
+// const age = 26
+//
+// console.log(typeof isProgrammer)
+// console.log(typeof name)
+// console.log(typeof age)
 
+// 5 Приоритет операторов
+const fullAge = 29
+const birthYear = 1991
+const currentYear = 2020
+
+const isFullAge = currentYear - birthYear >= fullAge
+console.log(isFullAge)
+
+// 6 условные операторы
