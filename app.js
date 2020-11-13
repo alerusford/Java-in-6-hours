6beb40e60d3173802feadc15805cda3eb9d07beb
Alexandr <alerusford@gmail.com>
2020-11-13 12:36:10 +0300

    master

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 // 1 переменные
+
+const firstName = 'Alexandr'
+const lastName = 'Ivanov'
+const age = 29
+const isProgrammer = true
+
+// 2 Мутирование
+//console.log('Имя человека: ' + firstName + ', а возраст человека: ' + age)
+
+//3 Операторы
+
