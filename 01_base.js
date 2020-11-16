3beceb037a3eb182baf8a78f9d05ac4c573ce827
Alexandr <alerusford@gmail.com>
2020-11-16 12:54:17 +0300

    Основы_the_end

---
+++ 01_base.js
@@ -0,0 +1,113 @@
+// 1 переменные
+// const firstName = 'Alexandr'
+// const lastName = 'Ivanov'
+//const age = 29
+//const isProgrammer = true
+
+// 2 Мутирование
+//console.log('Имя человека: ' + firstName + ', а возраст человека: ' + age)
+
+// 3 Операторы
+// const currentYear = 2020
+// const birthYear = 1991
+//const age = currentYear - birthYear
+//console.log(age)
+// const a = 10
+// const b = 5
+// let c = 32
+//c = c + a
+// c += a
+// console.log(c)
+
+// 4 типы данных
+// const isProgrammer = true
+// const name = 'Alexandr'
+// const age = 26
+//
+// console.log(typeof isProgrammer)
+// console.log(typeof name)
+// console.log(typeof age)
+
+// 5 Приоритет операторов
+// const fullAge = 29
+// const birthYear = 1991
+// const currentYear = 2020
+//
+// const isFullAge = currentYear - birthYear >= fullAge
+// console.log(isFullAge)
+
+// 6 условные операторы
+// const courseStatus = 'pending'
+// if (courseStatus === 'ready') {
+//     console.log('Курс уже готов и его можно проходить')
+// } else if (courseStatus === 'pending') {
+//     console.log('Курс пока находится в процессе разработки')
+// } else {
+//     console.log('Курс не получился')
+// }
+
+//const isReady = true
+// if (isReady) {
+//     console.log('Все готово!')
+// } else {
+//     console.log('Все не готово...')
+// }
+
+//Тернарное выражение
+//isReady ? console.log('все готово!') : console.log ('все не готово....')
+
+// 7 булевая логика
+// https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Operators/Логические_операторы
+// console.log(true && false)
+// console.log(true || false)
+// console.log(!true)
+
+// 8 Функции
+// function calculateAge(year) {
+//     return 2020 - year
+// }
+//
+// console.log(calculateAge(1992))
+
+// 9 Массивы
+// https://youtu.be/Bluxbh9CaQ0?t=5336
+// const cars = ['Мазда', 'Мерседес', 'Форд']
+// console.log(cars[0])
+//console.log(cars.length)
+
+// cars[0]='porsche'
+// console.log(cars)
+
+// 10 Циклы
+// const cars = ['Мазда', 'Мерседес', 'Форд', 'porsche']
+// for (let i = 0; i < cars.length; i++) {
+//     const car = cars[i]
+//     console.log(car)
+// }
+
+// for (let car of cars){
+//     console.log(car)
+// }
+
+// 11 Объекты
+const  person = {
+    firstName: 'Alexandr',
+    lastName: 'Ivanov',
+    year: 1993,
+    languages: ['Ru', 'En'],
+    hasWife: false,
+    greet: function (){
+        console.log('greet from person')
+    }
+}
+
+console.log(person.firstName)
+console.log(person['lastName'])
+const key = 'year'
+console.log(person[key])
+person.hasWife = true
+console.log(person)
+person.isProgrammer = true
+person.greet()
+
+// https://youtu.be/Bluxbh9CaQ0?t=6285
