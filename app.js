c6333dc40a7081a21e55c979b3085d705e18c630
Alexandr <alerusford@gmail.com>
2020-11-17 10:32:10 +0300

    Functions_start

---
+++ app.js
@@ -0,0 +1,16 @@
+// 04 Functions Функции
+// https://youtu.be/Bluxbh9CaQ0?t=9731
+
+// Function Declaration
+function greet(name){
+    console.log('Привет - ', name)
+}
+
+//Function Expression
+const greet2 = function greet2(name){
+    console.log('Привет - ', name)
+}
+
+greet('Лена')
+greet2('Лена2')
+
